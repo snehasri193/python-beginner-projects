31bcfff3c81a32cc0b7f787e96c51375f0e5d3c4
snehasri193 <guggilapusnehasrilatha2036.sse@saveetha.com>
2025-12-18 20:09:53 +0530

    Add files via upload
    Add basic conditional programs for login check and number ranges

---
+++ conditions/number_range.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+
+if num < 10:
+    print("Number is less than 10")
+elif num <= 50:
+    print("Number is between 10 and 50")
+else:
+    print("Number is greater than 50")
